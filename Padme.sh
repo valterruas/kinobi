b822870ea286d8b6782a93b415f6eaa9cbbb512c
Valter Ruas <valterruas@gmail.com>
2018-12-18 23:47:39 +0000

    Correção do Shell

--- Padme.sh
+++ Padme.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 for IP in 10 20 30 40
 do
-	ping -c2 192.168.99.$IP 2>&1 /dev/null && echo "Ok 192.168.99.$IP"
-	|| echo "Fail - 192.168.99.$IP"
+	ping -c2 192.168.99.$IP >- && echo "Ok - 192.168.99.$IP" || echo "Fail - 192.168.99.$IP"
 done
