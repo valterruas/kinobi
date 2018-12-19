e57ab53c9991ea6cfbd49c3b26ed569bb3cc341d
Valter Ruas <valterruas@gmail.com>
2018-12-19 00:21:21 +0000

    Altaração Branch Dev

--- Padme.sh
+++ Padme.sh
@@ -3,3 +3,4 @@ for IP in 10 20 30 40
 do
 	ping -c2 192.168.99.$IP >- && echo "Ok - 192.168.99.$IP" || echo "Fail - 192.168.99.$IP"
 done
+cowsay -f tux "Script Terminado"
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 *Repositorio Aula de Infra Agil*
+*Branch Dev*
+
 
 - Obtendo a FORÇA do git !!!
   - Bom, o git é ! - Yoda
